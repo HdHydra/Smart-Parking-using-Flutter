5d5cb4562dfe56bafd83fed8caad591be320351d
hdhydra <hdhydra101@gmail.com>
2023-03-29 17:40:21 +0530

    Changes to be committed: 	new file:   lib/values/archive.dart 	new file:   lib/values/constants.dart
    Changes not staged for commit:
    	modified:   lib/main.dart
    	modified:   lib/screens/forgot_page.dart
    	modified:   lib/screens/location_page.dart
    	modified:   lib/screens/login_page.dart
    	modified:   lib/screens/profile_page.dart
    	modified:   lib/screens/registration_page.dart
    	modified:   lib/values/constants.dart
    	modified:   lib/widgets/drawer.dart
    	modified:   macos/Flutter/GeneratedPluginRegistrant.swift
    	modified:   pubspec.lock
    	modified:   pubspec.yaml
    
    Untracked files:
    	android/app/google-services.json
    	lib/firebase_options.dart
    	lib/screens/main_page.dart

---
+++ lib/values/archive.dart
@@ -0,0 +1,83 @@
+//   [
+                        //   Polygon(
+                        //     points: polygons[0],
+                        //     label: 'A',
+                        //     isFilled: true,
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[1],
+                        //     label: 'B',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[2],
+                        //     label: 'C',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[3],
+                        //     label: 'D',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[4],
+                        //     label: 'E',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[5],
+                        //     label: 'F',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[6],
+                        //     label: 'G',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[7],
+                        //     label: 'H',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[8],
+                        //     label: 'I',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[9],
+                        //     label: 'J',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[10],
+                        //     label: 'K',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[11],
+                        //     label: 'L',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[12],
+                        //     label: 'M',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[13],
+                        //     label: 'N',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[14],
+                        //     label: 'O',
+                        //     color: Colors.blue,
+                        //   ),
+                        //   Polygon(
+                        //     points: polygons[15],
+                        //     label: 'P',
+                        //     color: Colors.blue,
+                        //   ),
+                        // ],
